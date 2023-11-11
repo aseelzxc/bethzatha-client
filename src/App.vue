c1500a5a372c438f95fc01d5e26cec7276156203
Russel Pengson <russelpengson123@gmail.com>
2023-11-10 22:35:31 -0800

    Rename main container

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import SideNavigation from './components/SideNavigation.vue'
 </script>
 
 <template>
 <SideNavigation/>
- <div class="wrap-all-the-things">
+ <div class="container">
     <RouterView/>    
   </div>
 </template>
