a105aa3c8dd5b07473c27e55cc6bfc88e3503781
Russel Pengson <russel@holmwoods.eu>
2023-12-01 21:01:32 +0800

    Finished styling header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,58 +1,91 @@
 <template>
-    <header class="flex-row">
-        <div class="logo-container flex-row">
-            <img src="../assets/images/logo.png" alt="" class="logo-image">
-            <h3>Bethzatha</h3>
+  <header class="flex-row">
+    <div class="logo-container flex-row">
+      <span class="logo">Bethzatha</span>
+    </div>
+    <div class="navigation-container flex-row">
+      <router-link to="/">
+        <div class="nav-item flex-row">
+          <i class="bx bxs-dashboard"></i>
+          <div>Dashboard</div>
         </div>
-        <div class="navigation-container flex-row">
-            <!-- <router-link to="/">
-                <span>Dashboard</span>
-            </router-link>
-            <router-link to="/add_patient">
-                <span>Manage patients</span>
-            </router-link>
-            <router-link to="/add_patient">
-                <span>Transactions</span>
-            </router-link> -->
+      </router-link>
+      <router-link to="/">
+        <div class="nav-item flex-row">
+          <i class="bx bxs-user"></i>
+          <div>Staffs</div>
         </div>
-        <div class="account-info-container flex-row">
-            <span class="text-bold">Test user</span>
-            <img src="../assets/icons/logout.svg" alt="">
+      </router-link>
+      <router-link to="/">
+        <div class="nav-item flex-row">
+          <i class="bx bx-clinic"></i>
+          <div>Doctors</div>
         </div>
-    </header>
+      </router-link>
+      <router-link to="/">
+        <div class="nav-item flex-row">
+          <i class="bx bx-test-tube"></i>
+          <div>Tests</div>
+        </div>
+      </router-link>
+      <router-link to="/">
+        <div class="nav-item flex-row">
+          <i class="bx bxs-home-heart"></i>
+          <div>Patients</div>
+        </div>
+      </router-link>
+      <router-link to="/">
+        <div class="nav-item flex-row">
+          <i class="bx bx-transfer-alt"></i>
+          <div>Transactions</div>
+        </div>
+      </router-link>
+    </div>
+  </header>
 </template>
-<style lang="scss" scoped>
-header{
-    padding: 0.5rem 4rem;
-    justify-content: space-evenly;
-    align-items: center;
-    gap: 4rem;
 
-    .logo-container{
-        gap: 0.5rem;
-        .logo-image{
-            height: 4rem;
-        }
-    }
-    .navigation-container{
-        flex-grow: 1;
-        gap: 2rem;
-        a{
-            color: inherit;
-            text-decoration: none;
-        }
-        span{
-            cursor: pointer;
-        }
-    }
-    .account-info-container{
-        gap: 1.5rem;
-        align-items: center;
-          
-        img{
-            width: 1.5rem;
-        }
-    }
+<style lang="scss" scoped>
+@import "../styles/colors.scss";
+
+header {
+  font-family: "Poppins", sans-serif;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  padding: 1rem 0;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+  .logo {
+    color: $--bethzatha-blue;
+    font-size: 1.5rem;
+  }
+
+  .logo-container,
+  .navigation-container {
+    margin: 0 5rem;
+  }
+
+  .a {
+    justify-content: center;
+    align-items: center;
+  }
 }
 
+.navigation-container {
+  gap: 2rem;
+
+  a {
+    text-decoration: none;
+  }
+
+  .nav-item {
+    justify-content: center;
+    align-items: center;
+    gap: 0.2rem;
+    color: $--text-grey;
+  }
+  .nav-item:hover {
+    color: $--bethzatha-blue;
+  }
+}
 </style>
+<script setup lang="ts"></script>
