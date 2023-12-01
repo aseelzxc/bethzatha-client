68959f9025bb1e807791b61b67f4efaac063e3f4
Russel Pengson <russel@holmwoods.eu>
2023-12-02 01:55:53 +0800

    Add some elements in ManagePatients

--- src/views/ManagePatients.vue
+++ src/views/ManagePatients.vue
@@ -1,5 +1,14 @@
 <template>
-  <section class="patients-management"></section>
+  <section class="patients-management flex-row">
+    <div class="content-container">
+      <div class="navigation-button-container"></div>
+    </div>
+  </section>
 </template>
 <script setup lang="ts"></script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.patients-management {
+  max-width: 1200px;
+  border: 1px solid;
+}
+</style>
