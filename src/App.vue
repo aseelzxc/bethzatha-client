32221d398760f07422bd8bdd57d297ece7fec38d
Russel Pengson <russel@holmwoods.eu>
2023-12-01 20:52:22 +0800

    Imported boxicons directly

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
+  <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
 <Header/>
  <div class="container">
     <RouterView/>    
   </div>
 </template>
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import Header from './components/Header.vue'
 </script>
 <style scoped>
