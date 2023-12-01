e6df70d33cafe791cd2068c37b68edcaf5d4638e
Russel Pengson <russel@holmwoods.eu>
2023-12-01 20:52:11 +0800

    Remove unused import in Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,11 @@
 <template>
 <main>
 </main>
-  <div class="wrap-all-the-things">
-    <router-view/>    
+  <div class="main-container">
+    <router-view/>
   </div>
 </template>
 <script setup lang="ts">
-  import SideNavigation from '../components/SideNavigation.vue'
 </script>
 
 <style>
